3c4fd99c64238ea0a4de56022a0cabe9a59d6aa1
Pedro Viana Schroeder <pedrovs.contato@gmail.com>
2024-09-22 20:54:08 -0300

    test: create test script

---
+++ src/test.ts
@@ -0,0 +1,13 @@
+import { executeTransaction } from './main';
+
+// executeTransaction(
+//     0.0001,
+//     'NTCeFjn2or3BkYuGa7hPCJdFhNSAqpmsxbZs1WK4csA',
+//     '4tVjfLDhJ7XD7VR9si7nqjNWTAjU5vowNxV1hjPXGtbF'
+// );
+
+executeTransaction(
+    0.0001,
+    'kq6Hb9AUTqVDGwBTPBtU83zQgJq9y4D9ieudaTypump',
+    '9wtbMwKu2j3XgtNGEbwWfJAAi2U9znphHK6mQ5bXi4vH'
+);
