8cdae52dd2c20fdd9ecea58eec84501d2777c176
Pedro Viana Schroeder <pedrovs.contato@gmail.com>
2024-09-22 20:54:28 -0300

    fix: don't handle duplicated signatures

--- src/main.ts
+++ src/main.ts
@@ -1,274 +1,266 @@
 import {
-  LIQUIDITY_STATE_LAYOUT_V4,
-  LiquidityPoolKeys,
-  MAINNET_PROGRAM_ID,
-  MARKET_STATE_LAYOUT_V3,
-  LiquidityPoolInfo,
-  TOKEN_PROGRAM_ID,
-  Token,
-  TokenAmount,
-  Percent,
-  Liquidity,
-  WSOL,
-
-} from "@raydium-io/raydium-sdk";
-import { createSyncNativeInstruction, getOrCreateAssociatedTokenAccount, NATIVE_MINT } from "@solana/spl-token";
+    LIQUIDITY_STATE_LAYOUT_V4,
+    LiquidityPoolKeys,
+    MAINNET_PROGRAM_ID,
+    MARKET_STATE_LAYOUT_V3,
+    LiquidityPoolInfo,
+    TOKEN_PROGRAM_ID,
+    Token,
+    TokenAmount,
+    Percent,
+    Liquidity,
+    WSOL,
+} from '@raydium-io/raydium-sdk';
 import {
-  Connection,
-  Keypair,
-  PublicKey,
-  sendAndConfirmTransaction,
-  SystemProgram,
-  Transaction,
-  TransactionInstruction
-} from "@solana/web3.js";
-import bs58 from "bs58";
+    createSyncNativeInstruction,
+    getOrCreateAssociatedTokenAccount,
+    NATIVE_MINT,
+} from '@solana/spl-token';
+import {
+    Connection,
+    Keypair,
+    PublicKey,
+    sendAndConfirmTransaction,
+    SystemProgram,
+    Transaction,
+    TransactionInstruction,
+} from '@solana/web3.js';
+import bs58 from 'bs58';
 
 const BN = require('bn.js');
 
 const getPoolKeys = async (ammId: string, connection: Connection) => {
-  const ammAccount = await connection.getAccountInfo(new PublicKey(ammId));
-  if (ammAccount) {
-    const poolState = LIQUIDITY_STATE_LAYOUT_V4.decode(ammAccount.data);
-    const marketAccount = await connection.getAccountInfo(poolState.marketId);
-    if (marketAccount) {
-      const marketState = MARKET_STATE_LAYOUT_V3.decode(marketAccount.data);
-      const marketAuthority = PublicKey.createProgramAddressSync(
-        [
-          marketState.ownAddress.toBuffer(),
-          marketState.vaultSignerNonce.toArrayLike(Buffer, "le", 8),
-        ],
-        MAINNET_PROGRAM_ID.OPENBOOK_MARKET,
-      );
-      return {
-        id: new PublicKey(ammId),
-        programId: MAINNET_PROGRAM_ID.AmmV4,
-        status: poolState.status,
-        baseDecimals: poolState.baseDecimal.toNumber(),
-        quoteDecimals: poolState.quoteDecimal.toNumber(),
-        lpDecimals: 9,
-        baseMint: poolState.baseMint,
-        quoteMint: poolState.quoteMint,
-        version: 4,
-        authority: new PublicKey(
-          "5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1",
-        ),
-        openOrders: poolState.openOrders,
-        baseVault: poolState.baseVault,
-        quoteVault: poolState.quoteVault,
-        marketProgramId: MAINNET_PROGRAM_ID.OPENBOOK_MARKET,
-        marketId: marketState.ownAddress,
-        marketBids: marketState.bids,
-        marketAsks: marketState.asks,
-        marketEventQueue: marketState.eventQueue,
-        marketBaseVault: marketState.baseVault,
-        marketQuoteVault: marketState.quoteVault,
-        marketAuthority: marketAuthority,
-        targetOrders: poolState.targetOrders,
-        lpMint: poolState.lpMint,
-      } as unknown as LiquidityPoolKeys;
+    const ammAccount = await connection.getAccountInfo(new PublicKey(ammId));
+    if (ammAccount) {
+        const poolState = LIQUIDITY_STATE_LAYOUT_V4.decode(ammAccount.data);
+        const marketAccount = await connection.getAccountInfo(poolState.marketId);
+        if (marketAccount) {
+            const marketState = MARKET_STATE_LAYOUT_V3.decode(marketAccount.data);
+            const marketAuthority = PublicKey.createProgramAddressSync(
+                [
+                    marketState.ownAddress.toBuffer(),
+                    marketState.vaultSignerNonce.toArrayLike(Buffer, 'le', 8),
+                ],
+                MAINNET_PROGRAM_ID.OPENBOOK_MARKET
+            );
+            return {
+                id: new PublicKey(ammId),
+                programId: MAINNET_PROGRAM_ID.AmmV4,
+                status: poolState.status,
+                baseDecimals: poolState.baseDecimal.toNumber(),
+                quoteDecimals: poolState.quoteDecimal.toNumber(),
+                lpDecimals: 9,
+                baseMint: poolState.baseMint,
+                quoteMint: poolState.quoteMint,
+                version: 4,
+                authority: new PublicKey('5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1'),
+                openOrders: poolState.openOrders,
+                baseVault: poolState.baseVault,
+                quoteVault: poolState.quoteVault,
+                marketProgramId: MAINNET_PROGRAM_ID.OPENBOOK_MARKET,
+                marketId: marketState.ownAddress,
+                marketBids: marketState.bids,
+                marketAsks: marketState.asks,
+                marketEventQueue: marketState.eventQueue,
+                marketBaseVault: marketState.baseVault,
+                marketQuoteVault: marketState.quoteVault,
+                marketAuthority: marketAuthority,
+                targetOrders: poolState.targetOrders,
+                lpMint: poolState.lpMint,
+            } as unknown as LiquidityPoolKeys;
+        }
     }
-  }
 };
-
 
 const calculateAmountOut = async (
-  poolKeys: LiquidityPoolKeys,
-  poolInfo: LiquidityPoolInfo,
-  tokenToBuy: string,
-  amountIn: number,
-  rawSlippage: number,
+    poolKeys: LiquidityPoolKeys,
+    poolInfo: LiquidityPoolInfo,
+    tokenToBuy: string,
+    amountIn: number,
+    rawSlippage: number
 ) => {
-  let tokenOutMint = new PublicKey(tokenToBuy);
-  let tokenOutDecimals = poolKeys.baseMint.equals(tokenOutMint) 
-    ? poolInfo.baseDecimals 
-    : poolKeys.quoteDecimals;
-  let tokenInMint = poolKeys.baseMint.equals(tokenOutMint) 
-    ? poolKeys.quoteMint 
-    : poolKeys.baseMint;
-  let tokenInDecimals = poolKeys.baseMint.equals(tokenOutMint)
-    ? poolInfo.quoteDecimals
-    : poolInfo.baseDecimals;
+    let tokenOutMint = new PublicKey(tokenToBuy);
+    let tokenOutDecimals = poolKeys.baseMint.equals(tokenOutMint)
+        ? poolInfo.baseDecimals
+        : poolKeys.quoteDecimals;
+    let tokenInMint = poolKeys.baseMint.equals(tokenOutMint)
+        ? poolKeys.quoteMint
+        : poolKeys.baseMint;
+    let tokenInDecimals = poolKeys.baseMint.equals(tokenOutMint)
+        ? poolInfo.quoteDecimals
+        : poolInfo.baseDecimals;
 
-  const tokenIn = new Token(TOKEN_PROGRAM_ID, tokenInMint, tokenInDecimals);
-  const tknAmountIn = new TokenAmount(tokenIn, amountIn, false);
-  const tokenOut = new Token(TOKEN_PROGRAM_ID, tokenOutMint, tokenOutDecimals);
-  const slippage = new Percent(rawSlippage, 100);
-  return {
-    amountIn: tknAmountIn,
-    tokenIn: tokenInMint,
-    tokenOut: tokenOutMint,
-    ...Liquidity.computeAmountOut({
-      poolKeys,
-      poolInfo,
-      amountIn: tknAmountIn,
-      currencyOut: tokenOut,
-      slippage,
-    }),
-  };
+    const tokenIn = new Token(TOKEN_PROGRAM_ID, tokenInMint, tokenInDecimals);
+    const tknAmountIn = new TokenAmount(tokenIn, amountIn, false);
+    const tokenOut = new Token(TOKEN_PROGRAM_ID, tokenOutMint, tokenOutDecimals);
+    const slippage = new Percent(rawSlippage, 100);
+    return {
+        amountIn: tknAmountIn,
+        tokenIn: tokenInMint,
+        tokenOut: tokenOutMint,
+        ...Liquidity.computeAmountOut({
+            poolKeys,
+            poolInfo,
+            amountIn: tknAmountIn,
+            currencyOut: tokenOut,
+            slippage,
+        }),
+    };
 };
-
 
 const makeSwapInstruction = async (
-  connection: Connection,
-  tokenToBuy: string,
-  rawAmountIn: number,
-  slippage: number,
-  poolKeys: LiquidityPoolKeys,
-  poolInfo: LiquidityPoolInfo,
-  keyPair: Keypair,
+    connection: Connection,
+    tokenToBuy: string,
+    rawAmountIn: number,
+    slippage: number,
+    poolKeys: LiquidityPoolKeys,
+    poolInfo: LiquidityPoolInfo,
+    keyPair: Keypair
 ) => {
-  const { amountIn, tokenIn, tokenOut, minAmountOut } =
-    await calculateAmountOut(
-      poolKeys,
-      poolInfo,
-      tokenToBuy,
-      rawAmountIn,
-      slippage,
+    console.log('Making Swap Instruction...');
+    const { amountIn, tokenIn, tokenOut, minAmountOut } = await calculateAmountOut(
+        poolKeys,
+        poolInfo,
+        tokenToBuy,
+        rawAmountIn,
+        slippage
     );
-  let tokenInAccount: PublicKey;
-  let tokenOutAccount: PublicKey;
+    let tokenInAccount: PublicKey;
+    let tokenOutAccount: PublicKey;
 
-  if (tokenIn.toString() == WSOL.mint) {
-    tokenInAccount = (
-      await getOrCreateAssociatedTokenAccount(
-        connection,
-        keyPair,
-        NATIVE_MINT,
-        keyPair.publicKey,
-      )
-    ).address;
-    tokenOutAccount = (
-      await getOrCreateAssociatedTokenAccount(
-        connection,
-        keyPair,
-        tokenOut,
-        keyPair.publicKey,
-      )
-    ).address;
-  } else {
-    tokenOutAccount = (
-        await getOrCreateAssociatedTokenAccount(
-          connection,
-          keyPair,
-          NATIVE_MINT,
-          keyPair.publicKey
-        )
-      ).address;
-      tokenInAccount = (
-        await getOrCreateAssociatedTokenAccount(
-          connection,
-          keyPair,
-          tokenIn,
-          keyPair.publicKey,
-        )
-      ).address;
-  }
-
-  const ix = new TransactionInstruction({
-    programId: new PublicKey(poolKeys.programId),
-    keys: [
-      { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
-      { pubkey: poolKeys.id, isSigner: false, isWritable: true },
-      { pubkey: poolKeys.authority, isSigner: false, isWritable: false },
-      { pubkey: poolKeys.openOrders, isSigner: false, isWritable: true },
-      { pubkey: poolKeys.baseVault, isSigner: false, isWritable: true },
-      { pubkey: poolKeys.quoteVault, isSigner: false, isWritable: true },
-      { pubkey: poolKeys.marketProgramId, isSigner: false, isWritable: false },
-      { pubkey: poolKeys.marketId, isSigner: false, isWritable: true },
-      { pubkey: poolKeys.marketBids, isSigner: false, isWritable: true },
-      { pubkey: poolKeys.marketAsks, isSigner: false, isWritable: true },
-      { pubkey: poolKeys.marketEventQueue, isSigner: false, isWritable: true },
-      { pubkey: poolKeys.marketBaseVault, isSigner: false, isWritable: true },
-      { pubkey: poolKeys.marketQuoteVault, isSigner: false, isWritable: true },
-      { pubkey: poolKeys.marketAuthority, isSigner: false, isWritable: false },
-      { pubkey: tokenInAccount, isSigner: false, isWritable: true },
-      { pubkey: tokenOutAccount, isSigner: false, isWritable: true },
-      { pubkey: keyPair.publicKey, isSigner: true, isWritable: false },
-    ],
-    data: Buffer.from(
-      Uint8Array.of(
-        9,
-        ...new BN(amountIn.raw).toArray("le", 8),
-        ...new BN(minAmountOut.raw).toArray("le", 8),
-      ),
-    ),
-  });
-  return {
-    swapIX: ix,
-    tokenInAccount: tokenInAccount,
-    tokenOutAccount: tokenOutAccount,
-    tokenIn,
-    tokenOut,
-    amountIn,
-    minAmountOut,
-  };
-};
-
-
-// 1111111111
-export const executeTransaction = async (swapAmountIn: number, tokenToBuy: string, ammId: string) => {
-  const connection = new Connection("https://api.mainnet-beta.solana.com");
-  const secretKey = bs58.decode("4uBeiZZxB9swkzuxeJ2uGm3jAiTFcoQtJarr5uDHZnFMiZEdMkSh9jGv32D3pcsNAk9Uwbp8YDPao7QTLTWGVzHB");
-  const keyPair = Keypair.fromSecretKey(secretKey);
-  // const ammId = "3XAeQAeiNPaKz4MhQ15JKLQz7YBuAPWDAJSGJb3YiRfj"; // Address of the SOL-USDC pool on mainnet
-  const slippage = 2; // 2% slippage tolerance
-  const currentTime1 = new Date().toLocaleTimeString();
-  console.log("Current Time1:", currentTime1);
-  const poolKeys = await getPoolKeys(ammId, connection);
-  const currentTime2 = new Date().toLocaleTimeString();
-  console.log("Current Time2:", currentTime2);
-  if (poolKeys) {
-    const poolInfo = await Liquidity.fetchInfo({ connection, poolKeys });
-    const currentTime3 = new Date().toLocaleTimeString();
-    console.log("Current Time3:", currentTime3);
-    const txn = new Transaction();
-    const {
-      swapIX,
-      tokenInAccount,
-      tokenIn,
-      amountIn
-    } = await makeSwapInstruction(
-      connection,
-      tokenToBuy,
-      swapAmountIn,
-      slippage,
-      poolKeys,
-      poolInfo,
-      keyPair,
-    );
-    const currentTime4 = new Date().toLocaleTimeString();
-    console.log("Current Time4:", currentTime4);
     if (tokenIn.toString() == WSOL.mint) {
-      // Convert SOL to Wrapped SOL
-      txn.add(
-        SystemProgram.transfer({
-          fromPubkey: keyPair.publicKey,
-          toPubkey: tokenInAccount,
-          lamports: amountIn.raw.toNumber(),
-        }),
-        createSyncNativeInstruction(tokenInAccount, TOKEN_PROGRAM_ID),
-      );
+        console.log(111111111111);
+        tokenInAccount = (
+            await getOrCreateAssociatedTokenAccount(
+                connection,
+                keyPair,
+                NATIVE_MINT,
+                keyPair.publicKey
+            )
+        ).address;
+        console.log(tokenInAccount);
+        tokenOutAccount = (
+            await getOrCreateAssociatedTokenAccount(
+                connection,
+                keyPair,
+                tokenOut,
+                keyPair.publicKey
+            )
+        ).address;
+        console.log(tokenOutAccount);
+    } else {
+        console.log(5555555555);
+        tokenOutAccount = (
+            await getOrCreateAssociatedTokenAccount(
+                connection,
+                keyPair,
+                NATIVE_MINT,
+                keyPair.publicKey
+            )
+        ).address;
+        console.log(6666666666);
+        tokenInAccount = (
+            await getOrCreateAssociatedTokenAccount(connection, keyPair, tokenIn, keyPair.publicKey)
+        ).address;
+        console.log(7777777777);
     }
-    txn.add(swapIX);
-    const currentTime5 = new Date().toLocaleTimeString();
-    console.log("Current Time5:", currentTime5);
-    const hash = await sendAndConfirmTransaction(connection, txn, [keyPair], {
-      skipPreflight: false,
-      preflightCommitment: "confirmed",
+
+    const ix = new TransactionInstruction({
+        programId: new PublicKey(poolKeys.programId),
+        keys: [
+            { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+            { pubkey: poolKeys.id, isSigner: false, isWritable: true },
+            { pubkey: poolKeys.authority, isSigner: false, isWritable: false },
+            { pubkey: poolKeys.openOrders, isSigner: false, isWritable: true },
+            { pubkey: poolKeys.baseVault, isSigner: false, isWritable: true },
+            { pubkey: poolKeys.quoteVault, isSigner: false, isWritable: true },
+            { pubkey: poolKeys.marketProgramId, isSigner: false, isWritable: false },
+            { pubkey: poolKeys.marketId, isSigner: false, isWritable: true },
+            { pubkey: poolKeys.marketBids, isSigner: false, isWritable: true },
+            { pubkey: poolKeys.marketAsks, isSigner: false, isWritable: true },
+            { pubkey: poolKeys.marketEventQueue, isSigner: false, isWritable: true },
+            { pubkey: poolKeys.marketBaseVault, isSigner: false, isWritable: true },
+            { pubkey: poolKeys.marketQuoteVault, isSigner: false, isWritable: true },
+            { pubkey: poolKeys.marketAuthority, isSigner: false, isWritable: false },
+            { pubkey: tokenInAccount, isSigner: false, isWritable: true },
+            { pubkey: tokenOutAccount, isSigner: false, isWritable: true },
+            { pubkey: keyPair.publicKey, isSigner: true, isWritable: false },
+        ],
+        data: Buffer.from(
+            Uint8Array.of(
+                9,
+                ...new BN(amountIn.raw).toArray('le', 8),
+                ...new BN(minAmountOut.raw).toArray('le', 8)
+            )
+        ),
     });
-    const currentTime6 = new Date().toLocaleTimeString();
-    console.log("Current Time6:", currentTime6);
-    console.log("Transaction Completed Successfully 🎉🚀.");
-    console.log(`Explorer URL: https://solscan.io/tx/${hash}`);
-  } else {
-    console.log(`Could not get PoolKeys for AMM: ${ammId}`);
-  }
+    return {
+        swapIX: ix,
+        tokenInAccount: tokenInAccount,
+        tokenOutAccount: tokenOutAccount,
+        tokenIn,
+        tokenOut,
+        amountIn,
+        minAmountOut,
+    };
 };
 
+export const executeTransaction = async (
+    swapAmountIn: number,
+    tokenToBuy: string,
+    ammId: string
+) => {
+    const connection = new Connection('https://api.mainnet-beta.solana.com');
+    const secretKey = bs58.decode(
+        '4uBeiZZxB9swkzuxeJ2uGm3jAiTFcoQtJarr5uDHZnFMiZEdMkSh9jGv32D3pcsNAk9Uwbp8YDPao7QTLTWGVzHB'
+    );
+    const keyPair = Keypair.fromSecretKey(secretKey);
+    const slippage = 2; // 2% slippage tolerance
 
-// executeTransaction(
-//     0.0001,
-//     // "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v" // USDC Address
-//     "8UZLqAhkYZs9McdPv4gDxScC2ARyBjYuYVcNY3nbpump"
-// )
+    const currentTime1 = new Date().toLocaleTimeString();
+    console.log('Current Time1:', currentTime1);
+    const poolKeys = await getPoolKeys(ammId, connection);
+    const currentTime2 = new Date().toLocaleTimeString();
+    console.log('Current Time2:', currentTime2);
+    if (poolKeys) {
+        const poolInfo = await Liquidity.fetchInfo({ connection, poolKeys });
+        const currentTime3 = new Date().toLocaleTimeString();
+        console.log('Current Time3:', currentTime3);
+        const txn = new Transaction();
+        const { swapIX, tokenInAccount, tokenIn, amountIn } = await makeSwapInstruction(
+            connection,
+            tokenToBuy,
+            swapAmountIn,
+            slippage,
+            poolKeys,
+            poolInfo,
+            keyPair
+        );
+        const currentTime4 = new Date().toLocaleTimeString();
+        console.log('Current Time4:', currentTime4);
+        if (tokenIn.toString() == WSOL.mint) {
+            // Convert SOL to Wrapped SOL
+            txn.add(
+                SystemProgram.transfer({
+                    fromPubkey: keyPair.publicKey,
+                    toPubkey: tokenInAccount,
+                    lamports: amountIn.raw.toNumber(),
+                }),
+                createSyncNativeInstruction(tokenInAccount, TOKEN_PROGRAM_ID)
+            );
+        }
+        txn.add(swapIX);
+        const currentTime5 = new Date().toLocaleTimeString();
+        console.log('Current Time5:', currentTime5);
+        const hash = await sendAndConfirmTransaction(connection, txn, [keyPair], {
+            skipPreflight: false,
+            preflightCommitment: 'confirmed',
+        });
+        const currentTime6 = new Date().toLocaleTimeString();
+        console.log('Current Time6:', currentTime6);
+        console.log('Transaction Completed Successfully 🎉🚀.');
+        console.log(`Explorer URL: https://solscan.io/tx/${hash}`);
+    } else {
+        console.log(`Could not get PoolKeys for AMM: ${ammId}`);
+    }
+};
--- src/websocket.ts
+++ src/websocket.ts
@@ -1,77 +1,85 @@
 import { Connection, PublicKey } from '@solana/web3.js';
 import { executeTransaction } from './main';
 
-const RAYDIUM_PUBLIC_KEY = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8";
-const HTTP_URL = "https://api.mainnet-beta.solana.com/"
-const WSS_URL = "wss://api.mainnet-beta.solana.com/"
+const RAYDIUM_PUBLIC_KEY = '675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8';
+const HTTP_URL = 'https://api.mainnet-beta.solana.com/';
+const WSS_URL = 'wss://api.mainnet-beta.solana.com/';
 const RAYDIUM = new PublicKey(RAYDIUM_PUBLIC_KEY);
-const INSTRUCTION_NAME = "initialize2";
-const SOL_TO_TRADE = 0.0001;
-
+const INSTRUCTION_NAME = 'initialize2';
+const SOL_TO_TRADE = 0.00001;
 const connection = new Connection(HTTP_URL, {
-    wsEndpoint: WSS_URL
+    wsEndpoint: WSS_URL,
 });
 
+const processedSignatures = new Set<string>();
+
 async function streamNewPools(connection: Connection, programAddress: PublicKey): Promise<void> {
-    console.log("Monitoring logs for program:", programAddress.toString());
-    connection.onLogs(
-        programAddress,
-        ({ logs, err, signature }) => {
-            if (err) return;
+    console.log('Monitoring logs for program:', programAddress.toString());
 
-            if (logs && logs.some(log => log.includes(INSTRUCTION_NAME))) {
-                console.log("Signature for 'initialize2':", `https://solscan.io/tx/${signature}`);
-                const mintData = fetchRaydiumMints(signature, connection);
-                mintData.then(async (data) => {
-                    if (data !== null) {
-                        executeTransaction(SOL_TO_TRADE, data.tokenAccount, data.ammId);
-                    }
-                });
-            }
-            "finalized"
+    connection.onLogs(programAddress, async ({ logs, err, signature }) => {
+        if (err) return;
+        if (processedSignatures.has(signature)) {
+            console.log(`Signature ${signature} already processed. Skipping...`);
+            return;
         }
-    );
-    }
+        if (logs && logs.some((log) => log.includes(INSTRUCTION_NAME))) {
+            processedSignatures.add(signature);
+            console.log("Signature for 'initialize2':", `https://solscan.io/tx/${signature}`);
+            const mintData = await fetchRaydiumMints(signature, connection);
+            if (mintData !== null) {
+                await executeTransaction(SOL_TO_TRADE, mintData.tokenAccount, mintData.ammId);
+            }
+        }
+    });
+}
 
-async function fetchRaydiumMints(txId: string, connection: Connection): Promise<{ ammId: string, tokenAccount: string } | null> {
+async function fetchRaydiumMints(
+    txId: string,
+    connection: Connection
+): Promise<{ ammId: string; tokenAccount: string } | null> {
     try {
-        const tx = await connection.getParsedTransaction(
-            txId,
-            {
-                maxSupportedTransactionVersion: 0,
-                commitment: 'confirmed'
-            });
+        const tx = await connection.getParsedTransaction(txId, {
+            maxSupportedTransactionVersion: 0,
+            commitment: 'confirmed',
+        });
 
         //@ts-ignore
-        const accounts = (tx?.transaction.message.instructions).find(ix => ix.programId.toBase58() === RAYDIUM_PUBLIC_KEY).accounts as PublicKey[];
+        const accounts = (tx?.transaction.message.instructions).find(
+            (ix) => ix.programId.toBase58() === RAYDIUM_PUBLIC_KEY
+            //@ts-ignore
+        ).accounts as PublicKey[];
         if (!accounts) {
-            console.log("No accounts found in the transaction.");
+            console.log('No accounts found in the transaction.');
             return null;
         }
-    
+
         const tokenAIndex = 8;
         const tokenBIndex = 9;
         const ammdIdIndex = 4;
-    
+
         const tokenAAccount = accounts[tokenAIndex];
         const tokenBAccount = accounts[tokenBIndex];
         const ammId = accounts[ammdIdIndex].toString();
-    
+
         const displayData = [
-            { "Token": "A", "Account Public Key": tokenAAccount.toBase58() },
-            { "Token": "B", "Account Public Key": tokenBAccount.toBase58() }
+            { Token: 'A', 'Account Public Key': tokenAAccount.toBase58() },
+            { Token: 'B', 'Account Public Key': tokenBAccount.toBase58() },
         ];
 
         console.table(displayData);
-        if (tokenAAccount.toBase58() !== "So11111111111111111111111111111111111111112") {
-            return { ammId, "tokenAccount": tokenAAccount.toBase58() };
-        } else if (tokenBAccount.toBase58() !== "So11111111111111111111111111111111111111112") {
-            return { ammId, "tokenAccount":tokenBAccount.toBase58() };
+        let tokenAccount;
+        if (tokenAAccount.toBase58() !== 'So11111111111111111111111111111111111111112') {
+            tokenAccount = tokenAAccount.toBase58();
+        } else if (tokenBAccount.toBase58() !== 'So11111111111111111111111111111111111111112') {
+            tokenAccount = tokenBAccount.toBase58();
         } else {
             return null;
         }
+        console.log('AMM ID:', ammId);
+        console.log('Token Account:', tokenAccount);
+        return { ammId, tokenAccount };
     } catch {
-        console.log("Error fetching transaction:", txId);
+        console.log('Error fetching transaction:', txId);
         return null;
     }
 }
